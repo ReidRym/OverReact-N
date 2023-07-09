a16c6ddba74b448e5ecddf4efa33a1e36db0f525
Ryan R <irishreid55@gmail.com>
2023-07-08 23:01:26 -0700

    created app so its responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,67 @@
 import React from "react";
-import { Nav, Navbar,NavbarToggler,Collapse,NavItem } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const NavBarz = () => {
+const NavBar = () => {
   return (
-    <Navbar bg="primary" className= "navFlex" variant="dark">
-      <Navbar.Brand href="#">Ryan Reid</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link as={Link} to="/" active>
-          Home
-          <span className="sr-only"></span>
-        </Nav.Link>
-        <Nav.Link as={Link} to="/about">
-          About
-        </Nav.Link>
-        <Nav.Link as={Link} to="/portfolio">
-          Portfolio
-        </Nav.Link>
-        <Nav.Link as={Link} to="/contact">
-          Contact
-        </Nav.Link>
-        <Nav.Link as={Link} to="/resume">
-          Resume
-        </Nav.Link>
-      </Nav>
+    <Navbar
+      bg="primary"
+      className="navFlex"
+      variant="dark"
+      expand="lg"
+      style={{
+        backgroundColor: "#FF0000",
+        boxShadow: "0px 0px 10px #000000",
+        borderRadius: "10px",
+      }}
+    >
+      <Navbar.Brand
+        href="#"
+        style={{
+          color: "#FF0000",
+          fontWeight: "bold",
+          fontSize: "1.5rem",
+          textTransform: "uppercase",
+          letterSpacing: "2px",
+          border: "none",
+          boxShadow: "none",
+        }}
+      >
+        Ryan Reid
+        <img
+          src={require('../images/war2.png')}
+          alt=""
+          style={{
+            width: "30px",
+            marginLeft: "10px",
+            border: "none",
+            boxShadow: "none",
+          }}
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/" active>
+            Home
+            <span className="sr-only"></span>
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about">
+            About
+          </Nav.Link>
+          <Nav.Link as={Link} to="/portfolio">
+            Portfolio
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contact">
+            Contact
+          </Nav.Link>
+          <Nav.Link as={Link} to="/resume">
+            Resume
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
 
-export default NavBarz;
+export default NavBar;
